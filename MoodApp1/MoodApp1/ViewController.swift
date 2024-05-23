fa9b29312303a4562f121b652d03f12c5748cc35
RezaKhibari <160886458+RezaKhibari@users.noreply.github.com>
2024-05-23 13:07:02 -0400

    Initial mood app

---
+++ MoodApp1/MoodApp1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoodApp1
+//
+//  Created by Alireza Khibari on 2024-05-23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
