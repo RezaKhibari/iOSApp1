693f02d7f01ee9ea7b5fc0f2afada19573041d0c
RezaKhibari <160886458+RezaKhibari@users.noreply.github.com>
2024-05-24 07:50:12 -0400

    IOS assingment 1

--- MoodApp1/MoodApp1/ViewController.swift
+++ MoodApp1/MoodApp1/ViewController.swift
@@ -8,25 +8,69 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var slider: UISlider!
 
+
+    var currentValue: Int = 0
+    var randomValue = 0
+    var slideMood="Happy"
+    var randomMood="Happy"
+
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+      super.viewDidLoad()
+      currentValue = lroundf(slider.value)
+      randomValue = Int.random(in: 1...100)
+
     }
 
+
     @IBAction func showAlert() {
+        
+        if (currentValue > 0  && currentValue < 20) {
+            slideMood = "Sad"
+        } else if ( currentValue > 19 && currentValue < 40) {
+            slideMood = "Neutral"
+        } else if ( currentValue > 39 && currentValue < 60) {
+            slideMood = "Happy"
+        } else if ( currentValue > 59 && currentValue < 80) {
+            slideMood = "Stressed"
+        } else if ( currentValue > 79 && currentValue < 101) {
+            slideMood = "Excited"
+        }
+        
+        if (randomValue > 0  && randomValue < 20) {
+            randomMood = "Sad"
+        } else if ( randomValue > 19 && randomValue < 40) {
+            randomMood = "Neutral"
+        } else if ( randomValue > 39 && randomValue < 60) {
+            randomMood = "Happy"
+        } else if ( randomValue > 59 && randomValue < 80) {
+            randomMood = "Stressed"
+        } else if ( randomValue > 79 && randomValue < 101) {
+            randomMood = "Excited"
+        }
+        let message = "The Mood of the slider is: \(slideMood)" +
+                       "\nThe random mood is: \(randomMood)"
       let alert = UIAlertController(
-        title: "Hello, World",
-        message: "This is my first app!",
+        title: "Hello",
+        message: message,    // changed
         preferredStyle: .alert)
 
       let action = UIAlertAction(
-        title: "Awesome",
+        title: "OK",          // changed
         style: .default,
         handler: nil)
 
       alert.addAction(action)
+
       present(alert, animated: true, completion: nil)
+    }
+
+
+    @IBAction func sliderMoved(_ slider: UISlider) {
+        currentValue = lroundf(slider.value)
     }
 
 }
