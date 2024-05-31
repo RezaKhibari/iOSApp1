93073957a67e335210714e62727fd96838ad36af
RezaKhibari <160886458+RezaKhibari@users.noreply.github.com>
2024-05-30 22:46:25 -0400

    Update Assignment 2

--- MoodApp1/MoodApp1/ViewController.swift
+++ MoodApp1/MoodApp1/ViewController.swift
@@ -10,10 +10,18 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet var slider: UISlider!
+    
+    @IBOutlet var scoreLable: UILabel!
 
+    @IBOutlet var roundLable: UILabel!
 
     var currentValue: Int = 0
     var randomValue = 0
+    var score = 0
+    var round = 0
+    var slidePart = 1
+    var randomPart = 1
+    var different = 0
     var slideMood="Happy"
     var randomMood="Happy"
 
@@ -22,37 +30,74 @@ class ViewController: UIViewController {
       super.viewDidLoad()
       currentValue = lroundf(slider.value)
       randomValue = Int.random(in: 1...100)
-
+      updateLabels()
     }
 
 
     @IBAction func showAlert() {
         
+        round += 1
         if (currentValue > 0  && currentValue < 20) {
             slideMood = "Sad"
+            slidePart = 1
         } else if ( currentValue > 19 && currentValue < 40) {
             slideMood = "Neutral"
+            slidePart = 2
         } else if ( currentValue > 39 && currentValue < 60) {
             slideMood = "Happy"
+            slidePart = 3
         } else if ( currentValue > 59 && currentValue < 80) {
             slideMood = "Stressed"
+            slidePart = 4
         } else if ( currentValue > 79 && currentValue < 101) {
             slideMood = "Excited"
+            slidePart = 5
         }
         
         if (randomValue > 0  && randomValue < 20) {
             randomMood = "Sad"
+            randomPart = 1
         } else if ( randomValue > 19 && randomValue < 40) {
             randomMood = "Neutral"
+            randomPart = 2
         } else if ( randomValue > 39 && randomValue < 60) {
             randomMood = "Happy"
+            randomPart = 3
         } else if ( randomValue > 59 && randomValue < 80) {
             randomMood = "Stressed"
+            randomPart = 4
         } else if ( randomValue > 79 && randomValue < 101) {
             randomMood = "Excited"
+            randomPart = 5
         }
+        
+        if (randomPart == slidePart) {
+            different = 0
+        } else if (slidePart > randomPart) {
+            different = slidePart - randomPart
+        } else {
+            different = randomPart - slidePart
+        }
+        
+        switch different {
+        case 0:
+            score += 100
+        case 1:
+            score += 80
+        case 2:
+            score += 60
+        case 3:
+            score += 40
+        case 4:
+            score += 20
+        default:
+            score += 0
+        }
+        updateLabels()
+        
         let message = "The Mood of the slider is: \(slideMood)" +
-                       "\nThe random mood is: \(randomMood)"
+                                "\nThe random mood is: \(randomMood)" +
+                                "\nThe Score is: \(score)"
       let alert = UIAlertController(
         title: "Hello",
         message: message,    // changed
@@ -71,6 +116,11 @@ class ViewController: UIViewController {
 
     @IBAction func sliderMoved(_ slider: UISlider) {
         currentValue = lroundf(slider.value)
+    }
+    
+    func updateLabels() {
+        scoreLable.text = String(score)
+        roundLable.text = String(round)
     }
 
 }
